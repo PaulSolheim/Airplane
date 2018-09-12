e67391515b809118b8a71614af52c4a691317964
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-09-12 15:19:22 +0200

    Klar for 3.gang
    Klargjort til 3 kodekveld.

---
+++ Assets/Airplane/Code/Scripts/Input/IP_BaseAirplane_Input.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KodeKlubb
+{
+    public class IP_BaseAirplane_Input : MonoBehaviour
+    {
+        #region Variables
+        protected float pitch = 0f;
+        protected float roll = 0f;
+        protected float yaw = 0f;
+        protected float throttle = 0f;
+        protected int flaps = 0;
+        protected float brake = 0f;
+        #endregion
+
+        #region Properties
+        public float Pitch
+        {
+            get { return pitch; }
+        }
+
+        public float Roll
+        {
+            get { return roll; }
+        }
+
+        public float Yaw
+        {
+            get { return yaw; }
+        }
+
+        public float Throttle
+        {
+            get { return throttle; }
+        }
+
+        public float Flaps
+        {
+            get { return flaps; }
+        }
+
+        public float Brake
+        {
+            get { return brake; }
+        }
+        #endregion
+
+        #region Custom Methods
+        void HandleInput()
+        {
+            pitch = Input.GetAxis("Vertical");
+            roll = Input.GetAxis("Horizontal");
+
+            Debug.Log("Pitch: " + pitch + " - " + "Roll: " + roll);
+        }
+        #endregion
+
+        #region Builtin Methods
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleInput();
+        }
+        #endregion
+    }
+}
