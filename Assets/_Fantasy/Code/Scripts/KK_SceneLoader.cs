1f67b9497d621335895976e7f7b9aeafb0599001
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-11-14 15:27:59 +0100

    Asset Packs og Ignore
    Lagt til Assetpacks og Ignore. Jobber med Inventory.

---
+++ Assets/_Fantasy/Code/Scripts/KK_SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KodeKlubb
+{
+    public class KK_SceneLoader : MonoBehaviour
+    {
+        public string sceneName = "InventoryUI_Partial";
+        public LoadSceneMode mode = LoadSceneMode.Additive;
+
+        private void Start()
+        {
+            var active = SceneManager.GetSceneByName(sceneName);
+            if (active.isLoaded == false)
+            {
+                SceneManager.LoadScene(sceneName, mode);
+            }
+        }
+    }
+}
+
